98ec215af914d9a489842a6aacbd6630cd62cadb
mitburr <mitburr@gmail.com>
2019-03-07 23:31:24 -0600

    heroku deployment connection added

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,21 @@ var connection = mysql.createConnection({
   database: "burger_db"
 });
 
+if(process.env.JAWSDB_URL){
+    connection=mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+    connection=mysql.createConnection({
+        host:'localhost',
+        port:3306,
+        user:"root",
+        password:"root",
+        database:"burgers_db"
+    })
+}
+connection.connect(function(){
+    console.log("Connection ID: "+connection.threadId)
+})
 // Make connection.
 connection.connect(function(err) {
   if (err) {
